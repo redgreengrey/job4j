6d443fea77ccf97e1147a7f251869bc8ee37a730
redgreengrey <mailto: redbillybones@gmail.com>
2018-08-05 21:19:44 +0400

    3.1. Элементарный калькулятор.

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    private double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public void dev(double first, double second) {
+        this.result = first / second;
+    }
+
+    public void multiply(double first, double second) {
+        this.result = first * second;
+    }
+
+    public void subtract(double first, double second) {
+        this.result = second - first;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
+}
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -1,9 +1,9 @@
-package java.ru.job4j.calculator;
+package ru.job4j.calculator;
 
 import org.junit.Test;
 import ru.job4j.calculator.Calculator;
-
 import static org.hamcrest.core.Is.is;
+
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
@@ -19,7 +19,7 @@ public class CalculatorTest {
     @Test
     public void whenDiv2On2Then1() {
         Calculator calculator = new Calculator();
-        calculator.add(2D, 2D);
+        calculator.dev(2D, 2D);
         double result = calculator.getResult();
         double expected = 1D;
         assertThat(result, is(expected));
@@ -28,16 +28,16 @@ public class CalculatorTest {
     @Test
     public void whenSubtract2From2Then0() {
         Calculator calc = new Calculator();
-        calc.add(1D, 2D);
+        calc.subtract(1D, 2D);
         double result = calc.getResult();
-        double expected = 0D;
+        double expected = 1D;
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenMultiply2And2Then4() {
         Calculator calc = new Calculator();
-        calc.add(2D, 2D);
+        calc.multiply(2D, 2D);
         double result = calc.getResult();
         double expected = 4D;
         assertThat(result, is(expected));
