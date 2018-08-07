d121c5d0193141b7369470ea9595968c3d2bf034
redgreengrey <mailto: redbillybones@gmail.com>
2018-08-07 22:09:55 +0400

    4.2. Максимум из двух чисел

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maximum = new Max();
+        int result = maximum.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstMoreSecond() {
+        Max maximum = new Max();
+        int result = maximum.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
