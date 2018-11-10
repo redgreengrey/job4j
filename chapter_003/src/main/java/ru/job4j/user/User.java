71c50dcfd4f19fa8fbebd5c134af6136aa2d5113
redgreengrey <redbillybones@gmailcom>
2018-11-11 00:11:24 +0400

    Организовать сортировку User

--- chapter_003/src/main/java/ru/job4j/user/User.java
+++ chapter_003/src/main/java/ru/job4j/user/User.java
@@ -2,10 +2,11 @@ package ru.job4j.user;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     private Integer id;
     private String name;
     private String city;
+    private int age;
 
     public int getId() {
         return id;
@@ -31,28 +32,56 @@ public class User {
         this.city = city;
     }
 
+    public User(Integer id, String name, String city, int age) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+
     public User(Integer id, String name, String city) {
         this.id = id;
         this.name = name;
         this.city = city;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id
-                && Objects.equals(name, user.name)
-                && Objects.equals(city, user.city);
+        return age == user.age &&
+                Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(city, user.city);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, city);
+        return Objects.hash(id, name, city, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int result = Integer.compare(this.age, o.age);
+        return result != 0 ? result : this.name.compareTo(o.name);
     }
 }
