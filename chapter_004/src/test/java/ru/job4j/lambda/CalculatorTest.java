d3c091e0ce0736d22175a2db7253606c5f582371
redgreengrey <redbillybones@gmailcom>
2018-12-24 15:07:00 +0400

    004-0.2 method references

--- chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -10,14 +10,26 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
+//    @Test
+//    public void whenAdd1Until3() {
+//        Calculator calc = new Calculator();
+//        List<Double> buffer = new ArrayList<>();
+//        calc.multiple(
+//                0, 3, 1,
+//                (value, index) -> (double) value + index,
+//                result -> buffer.add(result)
+//        );
+//        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+//    }
+
     @Test
     public void whenAdd1Until3() {
         Calculator calc = new Calculator();
         List<Double> buffer = new ArrayList<>();
         calc.multiple(
                 0, 3, 1,
-                (value, index) -> (double) value + index,
-                result -> buffer.add(result)
+                MathUtil::add, //static call
+                buffer::add // non-static call
         );
         assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
     }
