15ea76b75b67b838e9edb18e6c628a0d31a3253f
redgreengrey <redbillybones@gmailcom>
2019-01-09 11:13:11 +0400

    Осортировать департаменты

--- chapter_003/src/main/java/ru/job4j/sort/DepartmentSort.java
+++ chapter_003/src/main/java/ru/job4j/sort/DepartmentSort.java
@@ -6,39 +6,66 @@ public class DepartmentSort {
     /**
      *
      * @param source
-     * @return new treeSet in ascending order
+     * @return list of elements in natural order
      */
-    public Set<String> sort(Set<String> source) {
-        Set<String> result = new TreeSet<>();
-        result.addAll(source);
-        result.addAll(check(source));
-        return result;
+    public ArrayList<String> sort(ArrayList<String> source) {
+        check(source);
+        source.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return source;
     }
 
     /**
      *
      * @param source
-     * @return new treeSet in descending order
+     * @return list of elements in descending order
      */
-    public Set<String> descSort(Set<String> source) {
-        Set<String> result = new TreeSet<>(Comparator.reverseOrder());
-        result.addAll(source);
-        result.addAll(check(source));
-        return result;
+    public List<String> descSort(ArrayList<String> source) {
+        check(source);
+        source.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result;
+                if (o1.length() == o2.length()) {
+                    result = o2.compareTo(o1);
+                } else {
+                    int size = Math.min(o1.length(), o2.length());
+                    String first = o1.substring(0, size);
+                    String second = o2.substring(0, size);
+                    if (first.compareTo(second) == 0 && o1.length() > o2.length()) {
+                        result = 1;
+                    } else if (first.compareTo(second) == 0 && o1.length() < o2.length()) {
+                        result = -1;
+                    } else {
+                        result = o2.compareTo(o1);
+                    }
+                }
+                return result;
+            }
+        });
+        return source;
     }
 
-    private Set<String> check(Set<String> set) {
-        Set<String> result = new HashSet<>();
-        for (String str : set) {
-            for (int i = 0; i != str.length(); i++) {
-                if (str.charAt(i) == '\\') {
-                    String department = str.substring(0, i);
-                    if (!set.contains(department)) {
-                        result.add(department);
+    /**
+     * checks for departments that been missed
+     * @param list
+     */
+    private void check(ArrayList<String> list) {
+        for (int out = 0; out < list.size(); out++) {
+            String[] separated = list.get(out).split("\\\\");
+            if (separated.length > 1) {
+                String temp = separated[0];
+                for (int in = 1; in < separated.length; in++) {
+                    if (!list.contains(temp)) {
+                        list.add(temp);
                     }
+                    temp += "\\" + separated[in];
                 }
             }
         }
-        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/DepartmentSortTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/DepartmentSortTest.java
@@ -3,6 +3,8 @@ package ru.job4j.sort;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -12,12 +14,12 @@ import static org.junit.Assert.assertThat;
 
 public class DepartmentSortTest {
     private DepartmentSort depSorter;
-    private Set<String> directory;
+    private ArrayList<String> directory;
 
     @Before
     public void setDirectory() {
         this.depSorter = new DepartmentSort();
-        this.directory = new TreeSet<>();
+        this.directory = new ArrayList<String>();
         this.directory.add("K1\\SK1");
         this.directory.add("K2\\SK1\\SSK2");
         this.directory.add("K1\\SK2");
@@ -29,8 +31,8 @@ public class DepartmentSortTest {
 
     @Test
     public void whenSortAscending() {
-        Set<String> set = depSorter.sort(this.directory);
-        String[] result = set.toArray(new String[set.size()]);
+        List<String> list = depSorter.sort(this.directory);
+        String[] result = list.toArray(new String[list.size()]);
         String[] expect = new String[9];
         expect[0] = "K1";
         expect[1] = "K1\\SK1";
@@ -45,19 +47,44 @@ public class DepartmentSortTest {
     }
 
     @Test
-    public void whenSortDeccending() {
-        Set<String> set = depSorter.descSort(this.directory);
-        String[] result = set.toArray(new String[set.size()]);
+    public void whenSortDescending() {
+        List<String> list = depSorter.descSort(this.directory);
+        String[] result = list.toArray(new String[list.size()]);
         String[] expect = new String[9];
-        expect[0] = "K2\\SK1\\SSK2";
-        expect[1] = "K2\\SK1\\SSK1";
-        expect[2] = "K2\\SK1";
-        expect[3] = "K2";
-        expect[4] = "K1\\SK2";
-        expect[5] = "K1\\SK1\\SSK2";
-        expect[6] = "K1\\SK1\\SSK1";
-        expect[7] = "K1\\SK1";
-        expect[8] = "K1";
+        expect[0] = "K2";
+        expect[1] = "K2\\SK1";
+        expect[2] = "K2\\SK1\\SSK2";
+        expect[3] = "K2\\SK1\\SSK1";
+        expect[4] = "K1";
+        expect[5] = "K1\\SK2";
+        expect[6] = "K1\\SK1";
+        expect[7] = "K1\\SK1\\SSK2";
+        expect[8] = "K1\\SK1\\SSK1";
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortDescInStrangeManner() {
+        ArrayList<String> list = new ArrayList<>();
+        DepartmentSort sorter = new DepartmentSort();
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2");
+        list.add("K1\\SK1");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K1\\SK2");
+        list.add("K2\\SK1\\SSK2");
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("K2");
+        expected.add("K2\\SK1");
+        expected.add("K2\\SK1\\SSK2");
+        expected.add("K2\\SK1\\SSK1");
+        expected.add("K1");
+        expected.add("K1\\SK2");
+        expected.add("K1\\SK1");
+        expected.add("K1\\SK1\\SSK2");
+        expected.add("K1\\SK1\\SSK1");
+        sorter.descSort(list);
+        assertThat(list, is(expected));
     }
 }
